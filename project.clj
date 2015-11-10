753235a6c5f6d90a978ec0c5643d2fe82e7d01b1
Thomas G. Kristensen <thomas.g.kristensen@gmail.com>
2015-11-10 19:37:09 +0100

    Update cljsbuild usage and css

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [jayq "0.1.0-alpha2"]
 		 [hiccups "0.1.1"]]
-  :dev-dependencies [[lein-cljsbuild "0.1.6"]]
+  :plugins [[lein-cljsbuild "0.1.6"]]
   :cljsbuild {:builds [#_{:source-path "src-cljs"
                         :compiler {:optimizations :simple
                                    :pretty-print true
